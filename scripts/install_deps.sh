0aaebf2f95df5a1dbdba75dbf777a5126a3921a3
Ilyas Boudhaine <ilyasboudhaine1@gmail.com>
2026-05-11 15:04:05 +0200

    chore: ship vendored deps' LICENSE files

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -18,6 +18,7 @@ git clone --depth 1 --branch "$HNSWLIB_TAG" \
 rm -rf "$THIRD_PARTY/hnswlib"
 mkdir -p "$THIRD_PARTY/hnswlib"
 cp "$tmp/hnswlib/hnswlib/"*.h "$THIRD_PARTY/hnswlib/"
+cp "$tmp/hnswlib/LICENSE" "$THIRD_PARTY/hnswlib/"
 echo "$HNSWLIB_TAG" > "$THIRD_PARTY/hnswlib/VERSION"
 
 echo "Fetching CRoaring $CROARING_TAG amalgamation..."
@@ -28,6 +29,7 @@ rm -rf "$THIRD_PARTY/croaring"
 mkdir -p "$THIRD_PARTY/croaring"
 cp "$tmp/croaring/roaring.c" "$tmp/croaring/roaring.h" "$tmp/croaring/roaring.hh" \
    "$THIRD_PARTY/croaring/"
+cp "$tmp/croaring/LICENSE" "$THIRD_PARTY/croaring/"
 echo "$CROARING_TAG" > "$THIRD_PARTY/croaring/VERSION"
 
 echo "Done. Dependencies refreshed under $THIRD_PARTY."
